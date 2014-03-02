baed62c132e6f1cc8bbbc19f3ff13b29b640eadc
isaacs <i@izs.me>
2014-03-01 18:42:17 -0800

    also poll stats, because it's not watching properly on OS X where I need it to

--- filewatcherthing.js
+++ filewatcherthing.js
@@ -6,12 +6,14 @@ var spawn = require('child_process').spawn
 var args = process.argv.slice(2)
 
 // first arg is file/folder to watch
-var watchee = path.resolve(args.shift())
+var watchee = path.resolve(args.shift() || '')
 var cmd = args.shift()
 var usage = 'filewatcherthing <file or folder> <command> [ command args... ]'
 
-if (!watchee || !cmd)
-  throw new Error(usage)
+if (!watchee || !cmd) {
+  console.error(usage)
+  process.exit(1)
+}
 
 // Make sure it exists, and we can look at it.
 fs.statSync(watchee)
@@ -30,7 +32,10 @@ function run() {
 }
 
 function watch(watchee) {
+  console.error('watching', watchee)
   fs.watch(watchee, onwatch)
+  statPoll(watchee)
+
   // on osx, dir watching only catches rename events
   fs.readdir(watchee, function(er, children) {
     if (er) return;
@@ -42,11 +47,32 @@ function watch(watchee) {
   })
 }
 
+var stats = {}
+function statPoll(watchee) {
+  fs.stat(watchee, function(er, st) {
+    if (running)
+      return done()
+
+    if (er)
+      st = { mtime: new Date() }
+
+    var m = st.mtime.toISOString()
+    if (m !== stats[watchee])
+      run()
+
+    stats[watchee] = m
+    done()
+  })
+
+  function done() {
+    setTimeout(statPoll.bind(null, watchee), 1000)
+  }
+}
+
 function onwatch(event, file) {
-  if (running) return
   console.error(event, file)
+  if (running) return console.error('already running')
   run()
 }
 
 watch(watchee)
-
